293f2065d02499d05601734f02619e72cd7a5f79
ajaythekkiniyil <ajaythekkiniyi@gmail.com>
2022-10-19 15:31:57 +0530

    second commiit

---
+++ const.js
@@ -0,0 +1,3 @@
+const paymentStatusUrl = 'http://localhost:3000/payment-status'
+
+exports.paymentStatusUrl = paymentStatusUrl
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ POST URL: https://test.payu.in/_payment
 For PayU Production (LIVE) Server:
 POST URL: https://secure.payu.in/_payment
 */
+const urls = require('./const')
+
+
 var express = require('express');
 var session = require('express-session');
 var app = express();
@@ -23,76 +26,82 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname);
 
-//Unique merchant key provided by PayU along with salt. Salt is used for Hash signature 
-//calculation within application and must not be posted or transfered over internet. 
-var key = "Tg6kAm";
-var salt = "Gn6krnzbRwZghS2my9BslnlPTgfxwb5c";
+require('dotenv').config()
 
-//Generate random txnid
-app.get('/', function (req, res) {
+// key and salt stored in env file
+var key = process.env.PAYU_KEY
+var salt = process.env.PAYU_SALT
+
+app.post('/', function (req, res) {
+	// creating txnid
 	var ord = JSON.stringify(Math.random() * 1000);
 	var i = ord.indexOf('.');
 	txnid = 'ORD' + ord.substring(0, i);
-	res.send({ txnid, key })
-});
 
-app.post('/', function (req, res) {
-	console.log(req.body);
-	// txnid missing
-	const data = req.body
 	const udf5 = 'PayUBiz_NODE_JS_KIT'
+	const data = req.body
+
 	//generate hash with mandatory parameters and udf5
-	// var cryp = crypto.createHash('sha512');
-	// var text = key + '|' + txnid + '|' + data.updatedPrice + '|' + data.bookDetails.productinfo + '|' + data.address.firstname + '|' + data.address.email + '|||||' + udf5 + '||||||' + salt;
-	// cryp.update(text);
-	// var hash = cryp.digest('hex');
-	// res.setHeader("Content-Type", "text/json");
-	// res.setHeader("Access-Control-Allow-Origin", "*");
-	// res.send(hash)
+	var cryp = crypto.createHash('sha512');
+	var text = key + '|' + txnid + '|' + data.updatedPrice + '|' + data.bookDetails.bookTitle + '|' + data.address.firstname + '|' + data.address.email + '|||||' + udf5 + '||||||' + salt;
+	cryp.update(text);
+	var hash = cryp.digest('hex');
+	res.setHeader("Content-Type", "text/json");
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.send({txnid,key,hash})
 });
 
-app.post('/success', function (req, res) {
-	var verified = 'No';
-	var txnid = req.body.txnid;
-	var amount = req.body.amount;
-	var productinfo = req.body.productinfo;
-	var firstname = req.body.firstname;
-	var email = req.body.email;
-	var udf5 = req.body.udf5;
-	var mihpayid = req.body.mihpayid;
-	var status = req.body.status;
-	var resphash = req.body.hash;
-	var additionalcharges = "";
+app.post('/payment-status', function (req, res) {
+	if(req.body.status === 'failure'){
+		res.redirect(urls.paymentStatusUrl)
+	}
+	// const error_Message = req.body.error_Message
+	// const unmappedstatus = req.body.unmappedstatus
+	// const status = req.body.status
+
+	// var verified = 'No';
+	// var txnid = req.body.txnid;
+	// var amount = req.body.amount;
+	// var productinfo = req.body.productinfo;
+	// var firstname = req.body.firstname;
+	// var email = req.body.email;
+	// var udf5 = req.body.udf5;
+	// var mihpayid = req.body.mihpayid;
+	// var status = req.body.status;
+	// var resphash = req.body.hash;
+	// var additionalcharges = "";
 
 	//Calculate response hash to verify	
-	var keyString = key + '|' + txnid + '|' + amount + '|' + productinfo + '|' + firstname + '|' + email + '|||||' + udf5 + '|||||';
-	var keyArray = keyString.split('|');
-	var reverseKeyArray = keyArray.reverse();
-	var reverseKeyString = salt + '|' + status + '|' + reverseKeyArray.join('|');
+	// var keyString = key + '|' + txnid + '|' + amount + '|' + productinfo + '|' + firstname + '|' + email + '|||||' + udf5 + '|||||';
+	// var keyArray = keyString.split('|');
+	// var reverseKeyArray = keyArray.reverse();
+	// var reverseKeyString = salt + '|' + status + '|' + reverseKeyArray.join('|');
 	//check for presence of additionalcharges parameter in response.
-	if (typeof req.body.additionalCharges !== 'undefined') {
-		additionalcharges = req.body.additionalCharges;
-		//hash with additionalcharges
-		reverseKeyString = additionalcharges + '|' + reverseKeyString;
-	}
+	// if (typeof req.body.additionalCharges !== 'undefined') {
+	// 	additionalcharges = req.body.additionalCharges;
+	// 	//hash with additionalcharges
+	// 	reverseKeyString = additionalcharges + '|' + reverseKeyString;
+	// }
 	//Generate Hash
-	var cryp = crypto.createHash('sha512');
-	cryp.update(reverseKeyString);
-	var calchash = cryp.digest('hex');
-	console.log('calchash' + calchash);
-	var msg = 'Payment failed for Hash not verified...<br />Check Console Log for full response...';
+	// var cryp = crypto.createHash('sha512');
+	// cryp.update(reverseKeyString);
+	// var calchash = cryp.digest('hex');
+	// console.log('calchash' + calchash);
+	// var msg = 'Payment failed for Hash not verified...<br />Check Console Log for full response...';
 	//Comapre status and hash. Hash verification is mandatory.
-	if (calchash == resphash)
-		msg = 'Transaction Successful and Hash Verified...<br />Check Console Log for full response...';
+	// if (calchash == resphash)
+	// 	msg = 'Transaction Successful and Hash Verified...<br />Check Console Log for full response...';
 
-	res.send('Success')
+	// res.send('Success')
 
 });
-app.post('/cancel', function (req, res) {
-	res.send('payment cancelled by user')
-})
-app.post('/failed', function (req, res) {
-	res.send('payment failed')
-})
 
-app.listen(3000);
+// app.post('/cancel', function (req, res) {
+// 	res.send('payment cancelled by user')
+// })
+// app.post('/failed', function (req, res) {
+// 	res.send('payment failed')
+// })
+
+const port = process.env.PORT || 3001
+app.listen(port, ()=>console.log('server started on port:' + port));
